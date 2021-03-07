8bd93b302210292af2caebf5dba819edffef7532
eupone <eupone03@gmail.com>
2021-03-07 22:29:50 +0100

    adding intent and new db system , cogs dosent work

--- bot.py
+++ bot.py
@@ -2,61 +2,82 @@ import discord
 from discord.ext import commands, tasks
 import os
 from dotenv import load_dotenv
-import pymongo
-from pymongo import MongoClient
+import sqlite3
 
 load_dotenv()
-cluster=MongoClient(os.getenv("MongoDB"))
-db=cluster["Quasar"]
-collection=db["prefix"]
 
-default_prefixes = ['>']
+conn = sqlite3.connect('quasar.db')
+cur = conn.cursor()
 
-
-
-ext=["owner", "autres"]
-
-
-
-
-async def get_prefix(bot, msg):
-    guild = collection.find_one({"guild":msg.guild.id})
-    if guild == None:
-        return default_prefixes
-    else:
-        Prefix=guild.get("prefixe")
-        return Prefix
-
-
-
-
-load_dotenv()
-bot = commands.Bot(command_prefix = get_prefix, description = "Bot multifonctions")
+async def get_prefix(bot,msg):
+    return cur.execute("SELECT prefix FROM config WHERE guildid = ?", msg.guild.id).fetchone()
 
 
 
 
 
-@bot.command(name="prefix")
-@commands.guild_only()
-async def setprefix(ctx, *, prefixes=""):
-    server = collection.find_one({"guild":ctx.guild.id})
-    if server == None:
-        newGuild={"guild":ctx.guild.id, "prefixe":""}
-        collection.insert_one(newGuild)
-    collection.update_one({"guild":ctx.guild.id}, {"$set":{"prefixe":prefixes}})
-    result=collection.find_one({"guild":ctx.guild.id})
-    newPrefix=result.get("prefixe")
-    await ctx.send(f"Le nouveau préfixe est {newPrefix}")
+
+intents = discord.Intents.default()
+intents.members = True
+intents.typing = True
+intents.presences = True
+intents.reactions = True
+
+
+bot = commands.Bot(intents=intents,command_prefix = str(get_prefix), description = "Bot multifonctions")
+
+
+
 
 @bot.event
 async def on_ready():
     print(f'\n\nLogged in as: {bot.user.name} - {bot.user.id}\nVersion: {discord.__version__}\n')
-    activity = discord.Game(name=">help | Développé par Veloxx#4705", type=3)
+    activity = discord.Game(name=">help | Développé par Veloxx#4705 et eupone#9975", type=3)
     await bot.change_presence(status = discord.Status.online, activity = activity)
     print(f"Successfully logged in and booted...!\n")
-    for k in ext:
-        bot.load_extension(k)
+    for fold in os.listdir("cogs"):
+        print(fold + "\n")
+        for files in os.listdir(fold):
+            print(files)
+            bot.load_extension(files)
     print("Extension load !\n\n")
+
+
+@bot.event
+async def on_command_error(event,ctx):   #error handler
+    await ctx.send(event)
+
+
+@bot.event
+async def on_guild_join(guild):
+    cur.execute("SELECT * FROM config WHERE guildid = ?",(int(guild.id),) )
+    exist = cur.fetchone()
+    if not exist:
+        cur.execute('''INSERT INTO config(guildid,prefix) VALUES (?,'>')''', ( int(guild.id), ) )
+        conn.commit()
+        print("new guild !")
+    for memb in guild.members :
+        if memb.id != bot.user.id:
+            cur.execute("SELECT * FROM user WHERE userid = ?",(int(memb.id),) )
+            exist = cur.fetchone()
+            if not exist and not memb.bot:
+                cur.execute('''INSERT INTO user(userid,guildid) VALUES (?,?)''',( int(memb.id),int(guild.id) ) )
+                conn.commit()
+
+
+"""
+@bot.command(name="prefix")
+@commands.guild_only()
+async def setprefix(ctx, *, prefixes=""):
+    server = collection.find_one({"guild":ctx.guild.id})
+    
+    collection.update_one({"guild":ctx.guild.id}, {"$set":{"prefixe":prefixes}})
+    result=collection.find_one({"guild":ctx.guild.id})
+    newPrefix=result.get("prefixe")
+    await ctx.send(f"Le nouveau préfixe est {newPrefix}")
+"""
+
+
+
 
 bot.run(os.getenv("TOKEN"))
